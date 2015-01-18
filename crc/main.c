a89aa782e581a8eb9bbf1083b1fb91cbecdbc4d2
allievo <paolo.astegno@gmail.com>
2015-01-18 22:10:21 +0100

    rectified another error

--- crc/main.c
+++ crc/main.c
@@ -19,7 +19,7 @@
 /*    'main.c' compares the traditional solution (256 items lookup table) with a	*/
 /*    faster solution (that requires a 65536 items lookup table);		            */
 /*    the faster solution was tested with a DSP and the time required to process a	*/
-/*    buffer of data was decreased by 80 percent;                         			*/
+/*    buffer of data was decreased by 40 percent;                         			*/
 /*    the algorithm requires that the buffer size must be an even value; 			*/
 /************************************************************************************/
 
